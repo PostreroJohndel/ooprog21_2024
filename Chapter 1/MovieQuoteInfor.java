0fa20904bfff26db69f35f285f80c23a5da07568
PostreroJohndel <johndelpostrero2021@gmail.com>
2024-08-14 20:39:45 +0800

    Update MovieQuoteInfor.java

--- Chapter 1/MovieQuoteInfor.java
+++ Chapter 1/MovieQuoteInfor.java
@@ -1,17 +1,10 @@
 public class MovieQuoteInfor {
 
     public static void main(String[] args) {
-      
-        // Display my Favorite Quote
-        System.out.println("\n\n\t\t\t\"Remember, with great power comes great responsibility.\"");
 
-        // Display the movie title
-        System.out.println("\n\t Movie: Spider-Man");
-
-        // Display the character who said the quote
-        System.out.println("\t Character: Uncle Ben (Ben Parker)");
-
-        // Display the year of the movie
-        System.out.println("\t Year: 2002");
+        System.out.println("\n\n\t\t\t\"Remember, with great power comes great responsibility.\"");       // Display my Favorite Quote
+        System.out.println("\n\t Movie: Spider-Man");                                                     // Display the movie title
+        System.out.println("\t Character: Uncle Ben (Ben Parker)");                                       // Display the character who said the quote
+        System.out.println("\t Year: 2002");                                                              // Display the year of the movie
     }
 }
