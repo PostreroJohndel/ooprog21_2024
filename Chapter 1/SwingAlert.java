f94bf3079e658780bd05eb5e18caf538f8052532
PostreroJohndel <johndelpostrero2021@gmail.com>
2024-08-21 21:33:54 +0800

    Create SwingAlert.java

---
+++ Chapter 1/SwingAlert.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+
+public class SwingAlert {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "BOSSING!!! Kumusta ang buhay-buhay?");     // Display the message in a dialog box
+    }
+}
