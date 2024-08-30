451075d2b2c63b8c54bb17b16dd5d6e617d0444f
PostreroJohndel <johndelpostrero2021@gmail.com>
2024-08-30 08:29:32 +0700

    warmup_sleepin.java

--- Chapter 1/warmup_sleepin.java
+++ Chapter 1/warmup_sleepin.java
@@ -6,12 +6,12 @@ public class warmup_sleepin {
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Is it Weekday? 'false' or 'true': ");
-        String weekdayInput = scan.nextLine();    // ari mag input
-        boolean weekday = Boolean.parseBoolean(weekdayInput);    // Convert string input to boolean
+        String weekday_Input = scan.nextLine();    // ari mag input
+        boolean weekday = Boolean.parseBoolean(weekday_Input);    // Convert string input to boolean
 
         System.out.print("Are you on vacation? 'false' or 'true': ");
-        String vacationInput = scan.nextLine();      // ari mag input
-        boolean vacation = Boolean.parseBoolean(vacationInput);      // Convert string input to boolean
+        String vacation_Input = scan.nextLine();      // ari mag input
+        boolean vacation = Boolean.parseBoolean(vacation_Input);      // Convert string input to boolean
 
         // Call the static method sleepIn
         boolean result = sleepIn(weekday, vacation);
