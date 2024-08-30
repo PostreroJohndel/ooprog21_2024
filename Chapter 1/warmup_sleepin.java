b85d709fe3a6c48b1e7df43eaf6fb79c572e78b2
PostreroJohndel <johndelpostrero2021@gmail.com>
2024-08-30 08:19:23 +0700

    warmup_sleepin.java

--- Chapter 1/warmup_sleepin.java
+++ Chapter 1/warmup_sleepin.java
@@ -2,26 +2,24 @@ import java.util.Scanner;
 
 public class warmup_sleepin {
 
-    
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        
-        System.out.print("Is it Weekday? 'false' or 'true': ");   
-        boolean weekday = Boolean.parseBoolean(scan.nextLine());    // Ang user ari mag input
+        System.out.print("Is it Weekday? 'false' or 'true': ");
+        String weekdayInput = scan.nextLine();    // ari mag input
+        boolean weekday = Boolean.parseBoolean(weekdayInput);    // Convert string input to boolean
 
         System.out.print("Are you on vacation? 'false' or 'true': ");
-        boolean vacation = Boolean.parseBoolean(scan.nextLine());      // Ang user ari mag input
+        String vacationInput = scan.nextLine();      // ari mag input
+        boolean vacation = Boolean.parseBoolean(vacationInput);      // Convert string input to boolean
 
-        // Call the static method sleepIn with the user input and print the result
+        // Call the static method sleepIn
         boolean result = sleepIn(weekday, vacation);
         System.out.println("Can you sleep? (" + weekday + ", " + vacation + ") -> " + result);
         
         scan.close();
     }
     
-    // It's a boolean because it has only two choices: true and false.
-    // The 2 parameters are weekday and vacation.
     public static boolean sleepIn(boolean weekday, boolean vacation) {
         return !weekday || vacation; // !weekday -> not weekday, || -> OR
     }
