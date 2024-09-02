be103d101dcfdd1ba593ef0ab2cd0652a434aca4
PostreroJohndel <johndelpostrero2021@gmail.com>
2024-09-02 22:33:52 +0800

    Update CarlysEventPrice.java

--- Chapter 2/CarlysEventPrice.java
+++ Chapter 2/CarlysEventPrice.java
@@ -8,37 +8,20 @@ public class CarlysEventPrice {
         double price = 35.00;
         double totalPrice;
 
-        
         System.out.println("\n\n\t\t\t\t***************************************");
         System.out.println("\t\t\t\t*  Carly's makes the food that makes  *");
         System.out.println("\t\t\t\t*            it a party.              *");
         System.out.println("\t\t\t\t***************************************");
 
-        
         System.out.printf("\n\n\tInput the number of guests attending the event: ");
-        while (true) {
-            if (scan.hasNextInt()) {
-                numGuest = scan.nextInt();
-                if (numGuest >= 0) {
-                    break;
-                } else {
-                    System.out.println("\tPlease enter a positive number.");
-                }
-            } else {
-                System.out.println("\tInvalid input! Please enter a valid number.");
-                scan.next();  
-            }
-            System.out.printf("\n\tInput the number of guests attending the event: ");
-        }
+        numGuest = scan.nextInt();
 
         totalPrice = price * numGuest;
 
-        
         System.out.printf("\n\tNumber of guests: %d\n", numGuest);
         System.out.printf("\tPrice per guest: $%.2f\n", price);
         System.out.printf("\tTotal price: $%.2f\n", totalPrice);
 
-        
         if (numGuest >= 50) {
             System.out.println("\n\tThe job is classified as a large event!");
         } else {
