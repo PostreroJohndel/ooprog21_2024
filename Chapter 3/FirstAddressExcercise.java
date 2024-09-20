59dff1f76fc7613be7091f7b022e91a43136a575
PostreroJohndel <johndelpostrero2021@gmail.com>
2024-09-20 20:39:46 +0800

    Create FirstAddressExcercise.java

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,19 @@
+public class FirstAddressExcercise{
+
+   public static void main(String[] args){
+   
+      displayAddress();        //invoking the method displayAddress()
+     
+   }
+   
+   public static void displayAddress(){
+   
+      System.out.println("\n\n\tRealme Mobile Telecommunications Corp., Ltd");
+      System.out.println("\tBuilding A, No. 18, Wanfeng Road,");
+      System.out.println("\tBeijing Economic and Technological Development Zone,");
+      System.out.println("\tBeijing, China 100176");
+      System.out.println("\tCompany: Realme");
+     
+   }
+  
+}
