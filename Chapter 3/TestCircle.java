4c4d00c54dfa3307d29cae72c1ad4833b38f345f
PostreroJohndel <johndelpostrero2021@gmail.com>
2024-10-04 09:11:44 +0700

    create TestCircle.java

--- Chapter 3/TestCircle.java
+++ Chapter 3/TestCircle.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class TestCircle {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         
         Circle a = new Circle();
         Circle b = new Circle();
@@ -14,5 +17,12 @@ public class TestCircle {
         System.out.println("\n\tThe Radius of the Circle B: " + b.getRadius());
         System.out.println("\tThe Diameter of the Circle B: " + b.getDiameter());
         System.out.println("\tThe Area of the Circle B: " + b.getArea());
+        
+        System.out.printf("\n\tInput a value for Circle C: ");
+        double number = scan.nextDouble();
+        c.setRadius(number);
+        System.out.println("\n\tThe Radius of the Circle C: " + c.getRadius());
+        System.out.println("\tThe Diameter of the Circle C: " + c.getDiameter());
+        System.out.println("\tThe Area of the Circle C: " + c.getArea());
     }
-}
+} 
